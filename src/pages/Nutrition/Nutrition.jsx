86021d6b943b450c7d9a44324f44dbb7d88feb62
CollantesSan <santiagocollantesnieto@hotmail.com>
2025-05-19 23:03:33 -0500

    modulos de home
    shop,blog,support,wellnes,nutrition,fitness, se agregaron como navigate pero aun se requiere crear las paginas como tal

---
+++ src/pages/Nutrition/Nutrition.jsx
@@ -0,0 +1,5 @@
+const BlogNutrition = () => {
+return <div style={{ padding: "2rem" }}><h1>Nutrition Articles</h1><p>Coming soon...</p></div>;
+};
+
+export default BlogNutrition;
---
+++ src/pages/Wellness/Wellness.jsx
@@ -0,0 +1,5 @@
+const BlogWellness = () => {
+return <div style={{ padding: "2rem" }}><h1>Wellness Articles</h1><p>Coming soon...</p></div>;
+};
+
+export default BlogWellness;
